bbc801db1b74f34002fd130fd8cc20e3de5b86c4
Kilian Reg <164734699+KilianKowalski@users.noreply.github.com>
2024-07-04 10:43:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -65,7 +65,8 @@ document.addEventListener('DOMContentLoaded', () => {
         document.getElementById('events').style.backgroundColor = event.target.value;
     });
 
-    const events = [
+    const events = [#
+        { name: '2. Stunde', time: '08:45' },
         { name: 'Pause', time: '09:30' },
         { name: '3. Stunde', time: '09:50' },
         { name: '4. Stunde', time: '10:35' },
