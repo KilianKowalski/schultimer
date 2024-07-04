176ac56192aa073e1c9df10a6b2f56321cc49791
Kilian Reg <164734699+KilianKowalski@users.noreply.github.com>
2024-07-04 11:53:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -66,6 +66,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     const events = [
+        { name: '2. Stunde', time: '08:45' },
         { name: 'Pause', time: '09:30' },
         { name: '3. Stunde', time: '09:50' },
         { name: '4. Stunde', time: '10:35' },
