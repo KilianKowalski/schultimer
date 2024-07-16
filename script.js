734bebeb5541659b3ae05e8c7d439469a914db36
Kilian Reg <164734699+KilianKowalski@users.noreply.github.com>
2024-07-16 12:39:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -133,7 +133,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const seconds = Math.floor((diff % 60000) / 1000);
 
         document.getElementById('event-timer').textContent = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-        document.querySelector('#events h2').textContent = `Zeit bis zu/r ${nextEvent.name}`;
+        document.querySelector('#events h2').textContent = `Zeit bis ${nextEvent.name}`;
 
         if (hours === 0 && minutes === 0 && seconds === 0) {
             schoolBell.play();
